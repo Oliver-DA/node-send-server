7f48677cbc48416d64ee1c0102af491f09ef38fc
Onda18 <bboyonda@gmail.com>
2021-10-15 19:29:38 -0400

    api

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.static('uploads'))
 app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/links", require("./routes/link"));
-app.use("/api/file", require("./routes/file"));
+app.use("api/file", require("./routes/file"));
 
 const port = process.env.PORT || 4000;
 
