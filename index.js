298ab64cb327ccaca9f3d6699126fcb600a0da58
Onda18 <bboyonda@gmail.com>
2021-10-15 19:20:17 -0400

    index.js

--- index.js
+++ index.js
@@ -5,11 +5,8 @@ const app = express();
 
 //Connect DataBase
 connectDB();
-const corsOptions = {
-    origin: `${process.env.FRONTEND_URL}`,
-}
 
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.use(express.json());
 app.use(express.static('uploads'))
